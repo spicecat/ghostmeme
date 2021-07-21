de86a5b58f5fd7dbb5d18995d8fd0107a21a4e6b
Jason Zheng <jasonz4200@gmail.com>
2021-07-21 16:26:12 -0500

    Add API key, create getAllMemes() API function

---
+++ src/containers/Chats.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react'
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
+import {
+    Button,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    TextField
+} from '@material-ui/core'
+
+import { getAllMemes } from '../services/memeService'
+
+export const Chats = () => {
+    const [memes, setMemes] = useState([])
+
+    const makePostRequest = async () => {
+        const response = await getAllMemes()
+        setMemes(response)
+
+    }
+
+    // useEffect(makePostRequest)
+
+    return (
+        <>
+            <Button variant='contained' color='primary' onClick={makePostRequest}>Get All Memes</Button>
+            <br /><br />
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>createdAt</TableCell>
+                            <TableCell>expiredAt</TableCell>
+                            <TableCell>description</TableCell>
+                            <TableCell>private</TableCell>
+                            <TableCell>imageUrl</TableCell>
+                            <TableCell>meme_id</TableCell>
+                            <TableCell>owner</TableCell>
+                            <TableCell>receiver</TableCell>
+                            <TableCell>likes</TableCell>
+                            <TableCell>replyTo</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {memes.map(meme => (
+                            <TableRow key={meme.meme_id}>
+                                <TableCell>{meme.createdAt}</TableCell>
+                                <TableCell>{meme.expiredAt}</TableCell>
+                                <TableCell>{meme.description}</TableCell>
+                                <TableCell>{meme.privates}</TableCell>
+                                <TableCell>{meme.imageUrl}</TableCell>
+                                <TableCell>{meme.meme_id}</TableCell>
+                                <TableCell>{meme.owner}</TableCell>
+                                <TableCell>{meme.receiver}</TableCell>
+                                <TableCell>{meme.likes}</TableCell>
+                                <TableCell>{meme.replyTo}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
+}
+
+export default Chats
--- src/services/memeService.js
+++ src/services/memeService.js
@@ -1,8 +1,35 @@
 import superagent from 'superagent'
 
-import { serverUrl } from '../var.js'
+import { serverUrl, apiUrl, apiKey } from '../var.js'
 
 export async function getMemes() {
     try { return (await superagent.get(serverUrl,)).body }
     catch (err) { console.log('Error') }
+}
+
+export const getAllMemes = async () => {
+    const URL = `${apiUrl}/memes`
+
+    try {
+        const response = await superagent.get(URL).set('key', apiKey)
+
+        const memesList = response.body.memes.map(meme => {
+            return {
+                createdAt: meme.createdAt,
+                expiredAt: meme.expiredAt,
+                description: meme.description,
+                private: meme.private,
+                imageUrl: meme.imageUrl,
+                meme_id: meme.meme_id,
+                owner: meme.owner,
+                receiver: meme.receiver,
+                likes: meme.likes,
+                replyTo: meme.replyTo
+            }
+        })
+
+        return memesList
+    } catch (err) {
+        console.error(err)
+    }
 }
