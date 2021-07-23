ec3fab50c262ea12e65eba8a27e2e4a3bb3f93be
Deshawn Sutton <monkamonka6@gmail.com>
2021-07-23 16:38:56 -0500

    notifications update

--- src/containers/Notifications.jsx
+++ src/containers/Notifications.jsx
@@ -3,10 +3,23 @@ import React from 'react'
 import Form from '../components/Form'
 import PaperContent from '../components/Paper'
 
+
 export const Notifications = () => {
     return (
         <>
         Notifications
+        <Alert variant="filled" severity="error">
+            This is an error alert — check it out!
+        </Alert>
+        <Alert variant="filled" severity="warning">
+            This is a warning alert — check it out!
+        </Alert>
+        <Alert variant="filled" severity="info">
+            This is an info alert — check it out!
+        </Alert>
+        <Alert variant="filled" severity="success">
+            This is a success alert — check it out!
+        </Alert>
         </>
     )
 }
