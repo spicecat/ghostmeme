571d7fa0bbc0121abcf631d915cb26252b5ec215
cr5459 <85703159+cr5459@users.noreply.github.com>
2021-07-31 18:31:11 -0500

    frontend attempt in stories

--- src/containers/Stories.jsx
+++ src/containers/Stories.jsx
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { getLocalUser } from '../services/userService'
 import { searchMemes } from '../services/memeService'
+import { redirect } from '../services/userService'
+
 const axios = require('axios')
 
-export const Stories1 = () => {
-  const [user, setUser] = useState({ loading: true })
+
+export const Stories1 = ({user}) => {
+  // const [user, setUser] = useState({ loading: true })
+  useEffect(() => { redirect(user) }, [user])
   const [UsersMemesArray, setUserMemesArray] = useState([])
   const [valid, setValid] = useState(false)
 
-  useEffect(() => {
-    const updateUser = async () => { setUser(await getLocalUser()) }
-    updateUser()
-  }, [])
+  // useEffect(() => {
+  //   const updateUser = async () => { setUser(await getLocalUser()) }
+  //   updateUser()
+  // }, [])
 
 
 
@@ -20,7 +24,7 @@ export const Stories1 = () => {
 const GetsUsersMemes = async () => { 
   try { 
   setUserMemesArray(await searchMemes({owner:user.user_id}))
-   setValid(true)
+  setValid(true)
    console.log('got it!')
  }
   catch{
@@ -29,9 +33,31 @@ const GetsUsersMemes = async () => {
  }
 const mapper = (ix) => { 
   return (
-    <li>
-      <p>dsc: {ix.description} </p> 
-    </li>
+  
+  
+    <article className="Post" >
+            <header>
+              <div className="Post-user">
+                <div className="Post-user-profilepicture">
+                  <img src={ix.imageUrl} alt="Me and my Friends" />
+                  <p>{ix.description}</p>
+                </div>
+                <div className="Post-user-nickname">
+                  <span>{ix.username}</span>
+                </div>
+              </div>
+            </header>
+            <div className="Post-image">
+              <div className="Post-image-bg">
+                <img alt="Icon Living" src={ix.imageUrl} />
+              </div>
+            </div>
+            <div className="Post-caption">
+              <strong>John D. Veloper </strong> Loving Educative!
+            </div>
+            
+          </article>
+         
   )
 
 }
@@ -42,7 +68,7 @@ const TitleItems = UsersMemesArray.map(mapper)
       <>
           <h1>{user.username}'s Stories!</h1> <br />
           <button onClick={GetsUsersMemes} > Get up to date on your posts!</button>
-          {valid && <ul> {TitleItems}</ul> }
+          {valid && ( <ul> {TitleItems}</ul>)  }
       </>
   )
 }
@@ -59,27 +85,27 @@ const TitleItems = UsersMemesArray.map(mapper)
 
 //       render() {
 
-//         return <article className="Post" ref="Post">
-//             <header>
-//               <div className="Post-user">
-//                 <div className="Post-user-profilepicture">
-//                   <img src="https://www.w3schools.com/images/picture.jpg" alt="Me and my Friends" />
-//                   <p>I went hiking in the mountains!</p>
-//                 </div>
-//                 <div className="Post-user-nickname">
-//                   <span>John Doe</span>
-//                 </div>
-//               </div>
-//             </header>
-//             <div className="Post-image">
-//               <div className="Post-image-bg">
-//                 <img alt="Icon Living" src="[https://cdn-images-1.medium.com/max/1200/1*dMSWcBZCuzyRDeMr4uE_og.png]" />
-//               </div>
-//             </div>
-//             <div className="Post-caption">
-//               <strong>John D. Veloper </strong> Loving Educative!
-//             </div>
+        // return <article className="Post" ref="Post">
+        //     <header>
+        //       <div className="Post-user">
+        //         <div className="Post-user-profilepicture">
+        //           <img src="https://www.w3schools.com/images/picture.jpg" alt="Me and my Friends" />
+        //           <p>I went hiking in the mountains!</p>
+        //         </div>
+        //         <div className="Post-user-nickname">
+        //           <span>John Doe</span>
+        //         </div>
+        //       </div>
+        //     </header>
+        //     <div className="Post-image">
+        //       <div className="Post-image-bg">
+        //         <img alt="Icon Living" src="[https://cdn-images-1.medium.com/max/1200/1*dMSWcBZCuzyRDeMr4uE_og.png]" />
+        //       </div>
+        //     </div>
+        //     <div className="Post-caption">
+        //       <strong>John D. Veloper </strong> Loving Educative!
+        //     </div>
             
-//           </article>
+        //   </article>
 //         }
 //     }
