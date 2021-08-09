ebdd01fbc4e7b54d8fa504ac0a0f1d66333b18ab
“spicecat” <ateh0701@gmail.com>
2021-08-09 18:52:30 -0500

    fixed profile picture

--- src/containers/Stories.jsx
+++ src/containers/Stories.jsx
@@ -2,10 +2,30 @@ import { useState, useEffect } from 'react'
 import { Paper, Typography } from '@material-ui/core'
 
 import Story from '../components/Story'
+import Search from '../components/Search'
 
-export default function Stories({ user, likes, updateLikes }) {
+export default function Stories({ user, friends, likes, updateLikes }) {
+
+  // const updateUsers = async query => searchUsers(friends, query)
+
 
   return <Paper className='paper' elevation={3}>
     <Story user={user} likes={likes} updateLikes={updateLikes} />
+    {/* {
+      friends && <Search
+        name='users'
+        headCells={[
+          { name: 'Profile Picture', prop: 'imageUrl' },
+          { name: 'Username', prop: 'username' },
+          { name: 'Email', prop: 'email' },
+          { name: 'Phone', prop: 'phone' },
+          { name: 'Friends', prop: 'friends' },
+          { name: 'Likes', prop: 'liked' }]}
+        action={updateUsers}
+        schema={userSearchSchema}
+        Component={User}
+        update={updateSelectedUser}
+      />
+    } */}
   </Paper>
 }
