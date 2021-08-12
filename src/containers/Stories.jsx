5eb6d2ea87ef6256e9568013e0083d2b707dfb85
“spicecat” <ateh0701@gmail.com>
2021-08-12 08:58:29 -0500

    stories merge fix

--- src/containers/Stories.jsx
+++ src/containers/Stories.jsx
@@ -1,8 +1,18 @@
 import { useState, useEffect } from 'react'
+<<<<<<< HEAD
 import { Paper } from '@material-ui/core'
 
 import Story from '../components/Story'
 import Search from '../components/Search'
+=======
+import { Table, TableCell, TableRow, Paper, Typography } from '@material-ui/core'
+
+import Story from '../components/Story'
+import Form from '../components/Form'
+
+import { createMeme } from '../services/memeService'
+import { commentSchema, storySchema } from '../services/schemas'
+>>>>>>> parent of 483f3ff (readded story post form)
 
 export default function Stories({ user, friends, storyMemes, updateMemes, updateLikes }) {
   const [memes, setMemes] = useState([])
@@ -19,6 +29,27 @@ export default function Stories({ user, friends, storyMemes, updateMemes, update
   useEffect(getStory, [selectedUserInfo, storyMemes])
 
   return <Paper className='paper' elevation={3}>
+<<<<<<< HEAD
     <Story {...{ user: selectedUserInfo, memes, updateMemes, updateLikes, local_id: user.user_id }} />
+=======
+    <Story user={user} likes={likes} updateLikes={updateLikes} />
+    <hr />
+    {/* <div className='chat-footer'>
+      <Form
+        name='Post Story'
+        action={handleCreateMeme}
+        schema={storySchema}
+        inline={3}
+      />
+    </div>
+    <div className='chat-footer'>
+      <Form
+        name='Post Comment'
+        action={handleCreateMeme}
+        schema={commentSchema}
+        inline={3}
+      />
+    </div> */}
+>>>>>>> parent of 483f3ff (readded story post form)
   </Paper>
 }
