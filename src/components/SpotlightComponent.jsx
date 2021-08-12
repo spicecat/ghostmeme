24134c4b7a15401c2014bb212cbd56bc0a5704d4
Jason Zheng <jasonz4200@gmail.com>
2021-08-12 15:24:07 -0500

    Allow guest users to access Spotlight view

--- src/components/SpotlightComponent.jsx
+++ src/components/SpotlightComponent.jsx
@@ -8,20 +8,21 @@ import Form from './Form'
 import { createMeme } from '../services/memeService'
 import { memeSchema } from '../services/schemas'
 
-export default function SpotlightComponent({ user: { user_id, username }, memes, updateMemes, updateLikes, local_id }) {
+// export default function SpotlightComponent({ user: { user_id }, memes, updateMemes, updateLikes}) {
+export default function SpotlightComponent({ memes, updateMemes, updateLikes}) {
 
-    const handleCreateMeme = async values => {
-        if (await createMeme(user_id, null, values)) await updateMemes()
-    }
+    // const handleCreateMeme = async values => {
+    //     if (await createMeme(user_id, null, values)) await updateMemes()
+    // }
 
     return <>
         <Typography className='chat-header' variant='h4'>Spotlight</Typography>
         <Grid container>
             {memes && orderBy(memes, 'createdAt', 'desc').map(meme => <Fragment key={meme.meme_id} >
-                <Chat {...{ meme, username, updateMemes, updateLikes, local_id, isLocal: meme.owner === local_id, type: 'spotlight' }} />
+                <Chat {...{ meme, updateMemes, updateLikes, isLocal: '', type: 'spotlight' }} />
             </Fragment >)}
         </Grid>
-        <hr />
+        {/* <hr />
         <div className='chat-footer'>
             <Form
                 name='Post Public Meme'
@@ -29,6 +30,6 @@ export default function SpotlightComponent({ user: { user_id, username }, memes,
                 schema={memeSchema}
                 inline={2}
             />
-        </div>
+        </div> */}
     </>
 }
