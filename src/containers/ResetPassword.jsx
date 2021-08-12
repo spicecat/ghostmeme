9ba9f9e0a3d08b0f0bafa777475fe75b2767ebf3
cr5459 <85703159+cr5459@users.noreply.github.com>
2021-08-12 07:26:13 -0500

    password reset page

--- src/containers/ResetPassword.jsx
+++ src/containers/ResetPassword.jsx
@@ -1,7 +1,8 @@
 import {useParams} from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Form from '../components/Form'
-import { ResetPasswordSchema } from '../services/schemas'
+// import { ResetPasswordSchema } from '../services/schemas'
+import Button from '@material-ui/core/Button'
 const {sha256} = require('crypto-hash')
 const axios = require('axios')
 
@@ -25,6 +26,7 @@ const [conf, setconf] = useState(null)
 const [passwordStr, setPasswordStrength] = useState(null) 
 const [valid, setvalid] = useState(false) 
 const [reset, setreset] = useState(false) 
+const [matching,setmatching] = useState('')
 
 const handleSubmit = (e) => {
     e.preventDefault() 
@@ -41,10 +43,22 @@ const handleSubmit = (e) => {
 
  something()
 
+//  const matchingcheck=() => { 
+//     if (password===conf) {setmatching('true')}
+// }
+
 useEffect(() => {
         const currentPasswordStrength = checkPasswordStrength(password)
         setPasswordStrength(currentPasswordStrength)
+        if (password===conf) {setmatching(true)} else {setmatching(false)}
+        console.log('ran')
       }, [password]);
+
+ useEffect(() => {
+        if (password===conf) {setmatching(true)} else {setmatching(false)}
+        console.log('ra2n')
+      }, [conf]);
+
 
 const onClickHandler = () => { //this will reset the password. This is the last thing to fill and then this requirement is complete
     axios.post('http://localhost:3030/users/updatePassword', {
@@ -54,6 +68,8 @@ const onClickHandler = () => { //this will reset the password. This is the last 
     setreset(true)
 }
 
+
+
     return (
         <>
         {!valid && <p> Invalid link. Please try again. </p>}
@@ -61,12 +77,16 @@ const onClickHandler = () => { //this will reset the password. This is the last 
         <div> 
         <h1>Reset Password</h1> 
         <form onSubmit={handleSubmit} className="FormInputsFP">
-           <label htmlFor="newpassword">New password</label>
-           <input onChange={(e) => setpassword(e.target.value)} id='new password'/> <br/>
+           <label htmlFor="newpassword">New password</label> <br/>
+           <input type = "password" onChange={(e) => setpassword(e.target.value)} id='new password'/> <br/>
            <p> Password strength: {passwordStr} </p>
-           <label htmlFor="confirm password">Comfirm Password</label> 
-           <input onChange ={(e) => setconf(e.target.value)} id='confirmpassword'/> 
-           <button onClick= {onClickHandler} disabled={passwordStr==='weak' || password!== conf } > Reset Password</button>
+           {passwordStr==='weak' && <p>Password must contain at least 11 characters</p>}
+           <label htmlFor="confirm password">Comfirm Password</label> <br/>
+           <input type = "password" onChange ={(e) => setconf(e.target.value)} id='confirmpassword'/> <br/> 
+           {matching && <p>Passwords match! </p>}
+           {!matching && <p>Passwords do not match! </p>}      
+                <br/>
+           <Button variant="contained" onClick= {onClickHandler} disabled={passwordStr==='weak' || password!== conf } > Reset Password</Button>
        </form>
        {reset && <p>If an there account associated with {params.email}, then its password has been successfully reset. Please wait at least 45 seconds before logging in.</p> }
        </div>  
